2c73f7726b5b28f728bc09e18d3a530f83f42dd1
柴建祥 <chaijianxiang@lanjingren.com>
2020-03-29 22:52:47 +0800

    base layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,39 @@
 <template>
   <div class="home">
-    Home
+    <Layout class="main_layout">
+      <Header class="main_header">Template_Generator</Header>
+      <Content class="main_content">Content</Content>
+      <Footer>Footer</Footer>
+    </Layout>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    // HelloWorld
   }
 };
 </script>
+
+<style lang="scss" scope>
+.home {
+  height: 100%;
+  .main_layout {
+    height: 100%;
+  }
+  .main_header {
+    height: 60px;
+    background: #ed6c00;
+    color: #fff;
+  }
+  .main_content {
+    // height: 80%;
+    background: #ccc;
+  }
+}
+</style>
