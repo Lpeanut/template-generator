ff91ce905a77d1b4d116127e07225ff3af0a694e
柴建祥 <chaijianxiang@lanjingren.com>
2020-04-26 20:47:39 +0800

    testing

--- src/views/test-page/index.vue
+++ src/views/test-page/index.vue
@@ -1,60 +1,121 @@
 
-      
     <template>
-    <div class="container" >
-      
-    <div class="left_cover" >
-      
-    </div>
-  
-    <div class="mid" >
-      
-    </div>
-  
-    <div class="right_cover" >
-      
-    </div>
-  
-    </div>
-  </template>
-  
-      <style>
-        
+      <div class="container" ><div class="fm" ><img class="fm_img"  /><img class="fm_bg" src="https://static2.ivwen.com/pdf/book/cdn/tpl_pics/501/yfm_1.png"  /><div class="fm_title" >如何看待原油期货结算价持续刷新新低？</div><div class="fm_author" >唐纳德·特朗普</div><img class="fm_logo_svg" src="https://static2.ivwen.com/pdf/book/cdn/tpl_pics/501/logo_fd.svg"  /></div><div class="mid" ><div class="mid_text" >如何看待原油期货结算价持续刷新新低😁历</div></div><div class="fd" ><img class="fd_bg" src="https://static2.ivwen.com/pdf/book/cdn/tpl_pics/501/yfd_1.png"  /><img class="fd_logo_svg" src="https://static2.ivwen.com/pdf/book/cdn/tpl_pics/501/logo_fm.svg"  /><div class="fd_qrcode_box" ></div><div class="fd_qrcode_text" >扫码阅读美篇原文</div><div class="fd_barcode_box" content="扫码阅读美篇原文" ></div><div class="fd_barcode_text" >B123456123456</div></div></div>
+    </template>
+    <style>
       .container{
-        
-    width: 396.3px;
-    height: 276px;background: #7c0505;position: relative;
-      }
-    
-
-      .left_cover{
-        
-    width: 178px;
-    height: 256px;
+    width: 3963px;
+    height: 2760px;background: #7C0505;position: relative;word-break: break-word;transform: scale(0.37757575758);transform-origin: top left;}
+.fm{
+    width: 1780px;
+    height: 2560px;
     position: absolute;
-    left: 10px;
-    top: 10px;background: green;
-      }
-    
-
-      .mid{
-        
-    width: 20px;
-    height: 256px;
+    left: 2083px;
+    top: 100px;}
+.fm_img{
+    width: 1780px;
+    height: 1335px;
     position: absolute;
-    left: 187.9px;
-    top: 10px;background: purple;
-      }
-    
-
-      .right_cover{
-        
-    width: 178px;
-    height: 256px;
+    left: 0px;
+    top: 535px;}
+.fm_bg{
+    width: 1780px;
+    height: 2560px;
     position: absolute;
-    left: 208.3px;
-    top: 10px;background: gold;
+    left: 0px;
+    top: 0px;}
+.fm_title{
+    width: 1780px;
+    height: 86px;
+    position: absolute;
+    left: 0px;
+    top: 2044px;
+    font-weight: 500;
+    font-size: 80px;
+    line-height: 86px;color: #fff;text-align: center;letter-spacing: 1px;}
+.fm_author{
+    width: 1780px;
+    height: 86px;
+    position: absolute;
+    left: 0px;
+    top: 2186px;
+    font-weight: 300;
+    font-size: 40px;
+    line-height: 50px;color: #fff;text-align: center;}
+.fm_logo_svg{
+    width: 390px;
+    height: 39px;
+    position: absolute;
+    left: 670px;
+    top: 2371px;}
+.mid{
+    width: 200px;
+    height: 2560px;
+    position: absolute;
+    left: 1879px;
+    top: 100px;}
+.mid_text{
+    width: 64px;
+    height: undefined;
+    position: absolute;
+    left: 68px;
+    top: 300px;
+    font-weight: 400;
+    font-size: 56px;
+    line-height: 66px;color: #FFF;}
+.fd{
+    width: 1780px;
+    height: 2560px;
+    position: absolute;
+    left: 100px;
+    top: 100px;}
+.fd_bg{
+    width: 1630px;
+    height: 2460px;
+    position: absolute;
+    left: 51px;
+    top: 50px;}
+.fd_logo_svg{
+    width: 572px;
+    height: 41px;
+    position: absolute;
+    left: 280px;
+    top: 280px;}
+.fd_qrcode_box{
+    width: 200px;
+    height: 200px;
+    position: absolute;
+    left: 782px;
+    top: 1921px;background: #FFF;}
+.fd_qrcode_text{
+    width: 200px;
+    height: 24px;
+    position: absolute;
+    left: 782px;
+    top: 2130px;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 24px;color: #FFF;text-align: center;}
+.fd_barcode_box{
+    width: 320px;
+    height: 110px;
+    position: absolute;
+    left: 722px;
+    top: 2245px;background: #FFF;}
+.fd_barcode_text{
+    width: 210px;
+    height: 24px;
+    position: absolute;
+    left: 776px;
+    top: 2365px;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 24px;color: #FFF;text-align: center;}
+
+    </style>
+    <script>
+      export default {
+        
       }
-    
-      </style>
-    
+    </script>
+  
